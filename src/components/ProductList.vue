f9b1f688b174195c3fc77b564584f9f98a65076c
lagunajonell <lagunajonell81@gmail.com>
2024-04-12 20:34:08 +0800

    product
    for product entry and product list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,15 +1,26 @@
 <template>
-    <div>
-
+    <div class="product-list">
+      <Product v-for="product in products" :key="product.id" :product="product" />
     </div>
-</template>
-
-<script>
-    export default {
-        
+  </template>
+  
+  <script>
+  import Product from './ProductEntry.vue';
+  
+  export default {
+    components: {
+      Product
+    },
+    data() {
+      return {
+        products: [
+          { id: 1, name: 'Product 1', description: 'Description of Product 1', price: 10 },
+          { id: 2, name: 'Product 2', description: 'Description of Product 2', price: 20 },
+          { id: 3, name: 'Product 3', description: 'Description of Product 3', price: 10 },
+          { id: 4, name: 'Product 4', description: 'Description of Product 4', price: 20 },
+          { id: 5, name: 'Product 5', description: 'Description of Product 5', price: 10 },
+        ]
+      }
     }
-</script>
-
-<style scoped>
-
-</style>
+  }
+  </script>
